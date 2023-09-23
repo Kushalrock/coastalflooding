101dbfc2abd68033178306db3980860da69e4fe9
Kushal Agrawal <64556649+Kushalrock@users.noreply.github.com>
2023-09-23 13:35:44 +0530

    Update api_config.dart

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
   static String newsApiGet =
-      "https://newsapi.org/v2/everything?q=flooding&sources=bbc-news&apiKey=9f517f1c91d84da29a5c28be2d609abf&pageSize=20&page=1";
+      "https://newsapi.org/v2/everything?q=flooding&sources=bbc-news&apiKey=4582825b55dd492093403e6be2b05e28&pageSize=20&page=1";
   static String predictHQToken = "96ybNGyQSK_cR6GRLq25b2H2ROxe4oVwFQ8n_UZ7";
   static String predictHq =
       "https://api.predicthq.com/v1/events?category=severe-weather,disasters&country=IN&active.gte=2023-07-01";
